7721230841181efdfc3d7d0892b9234d2b3813c1
Nischhalsubba <hinischalsubba@gmail.com>
2019-05-06 01:51:44 -0700

    test message

--- git introduction.js
+++ git introduction.js
@@ -18,3 +18,39 @@
 // git reset HEAD app/ -->> it removes the files from staging area
 
 // git diff (file path)
+
+
+
+URL
+//HTTPS -> email and password is required every time you push
+//SSH(Secure Shell) -> inside ssh folder (id_rsa)   and (id_rsa_pub)
+
+// git remote add origin (URL HERE!!)
+// origin= remote 
+
+
+// to change git clone folder name
+// git clone (URL) (NEW FOLDER NAME)
+
+
+
+
+
+
+//STEP TO FOLLOW FOR ADDING A REPO TO REMOTE
+
+// git init -> initialize git in project
+
+
+// Make changes to your files
+// git add file_name -< add files to stating area
+
+
+// after adding the files we add a commit message to let us know what we did
+// git commit -m "your message goes here" -> ADD commit message
+
+//WE add remote repository url to push our project to git stations(github/gitlab/gitbucket)\
+//
+
+
+
